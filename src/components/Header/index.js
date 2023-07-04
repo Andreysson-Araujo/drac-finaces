15503e521ab1d7270d7e06be981e7f60e6a0c6fd
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-07-04 09:06:08 -0500

    iniciando cards

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import * as C from "./styles"
 
-function Header() {
+const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <C.Container>
+        <C.Header>
+            <C.Title>Contole Financeiro</C.Title>
+        </C.Header>
+    </C.Container>
+  );
+};
 
 export default Header
